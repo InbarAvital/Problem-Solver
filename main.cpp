f52a9cd748f4150977ad15ad62135be7c74a5d9f
Inbar Avital <53995120+InbarAvital@users.noreply.github.com>
2020-01-22 00:32:53 +0200

    main.cpp upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by inbar on 21/01/2020.
+//
+
+
+#include "headers/MySerialServer.h"
+
+using namespace server_side::boot;
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    Main m;
+    m.main(argc, argv);
+}
