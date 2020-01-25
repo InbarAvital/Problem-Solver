9484e9f0febac700465773d5c35e2481048be05f
noamyakov <59260056+noamyakov@users.noreply.github.com>
2020-01-25 03:43:28 +0200

    Add files via upload

--- algorithms/Searcher.h
+++ algorithms/Searcher.h
@@ -14,10 +14,10 @@ class Searcher {
   // Input: searchable - A searching problem.
   // Output: A solution for this problem.
   // Solves the searching problem and returns (one of) its solution(s).
-  string search(Searchable<T> searchable) = 0;
+  virtual string search(Searchable<T> searchable) = 0;
 
   // Output: The number of nodes evaluated during the searching process.
-  int GetNumberOfNodesEvaluated() = 0;
+  virtual int GetNumberOfNodesEvaluated() = 0;
 };
 
 #endif //EX4_ALGORITHMS_SEARCHER_H_
