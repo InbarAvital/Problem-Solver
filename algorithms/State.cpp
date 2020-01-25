6c6238c35f62763f4782b26b79a17a2bb55c82c0
noamyakov <59260056+noamyakov@users.noreply.github.com>
2020-01-25 02:43:49 +0200

    Update State class

--- algorithms/State.cpp
+++ algorithms/State.cpp
@@ -5,6 +5,20 @@
 #include "State.h"
 
 template<class T>
-bool State<T>::operator==(const State &s) const {
+bool State<T>::operator==(const State<T> &s) const {
   return this->state == s.state;
+}
+
+template<class T>
+list<T> State<T>::BacktracePath() const {
+  list<T> path;
+
+  if (this->previous_state != nullptr) {
+    // Backtraces the path until the previous state.
+    path = this->previous_state->BacktracePath();
+  }
+
+  // Adds this state to the path.
+  path.push_back(this->state);
+  return path;
 }
--- algorithms/State.h
+++ algorithms/State.h
@@ -5,33 +5,48 @@
 #ifndef EX4__STATE_H_
 #define EX4__STATE_H_
 
+#include <list>
+#include <string>
+
+using namespace std;
+
 template <class T>
 class State {
  public:
   // Input: s - The state
   // Constructs a state.
-  State(T s): state(s) {}
+  State(T s): state(s), previous_state(nullptr) {}
 
   // Input: s - A state
   // Output: True or false whether this state is equal to the other state.
-  bool operator==(const State &s) const;
+  bool operator==(const State<T> &s) const;
 
-  // Input: c - The cost of the path leads to this state.
-  // Sets this cost as this state's cost.
-  void SetCost(double c) {
-    this->cost = c;
+  // Returns this state.
+  T GetState() const {
+    return this->state;
+  }
+
+  // Returns this state's cost - the cost of the path leads to this state.
+  double GetCost() const {
+    return this->cost;
   }
 
   // Input: s - The state before this state in the path leads to this state.
-  // Sets that state as this state's previous state.
-  void SetPreviousState(State<T> s) {
-    this->previous_state = state;
+  //        local_cost - The cost of the last action in the path leads to this state.
+  // Sets that state as this state's previous state and that path's cost.
+  void SetPreviousState(State<T> *s, double local_cost) {
+    this->previous_state = s;
+    this->cost = s->cost + local_cost;
   }
+
+  // Output: The path leads to this state.
+  // Backtraces that path and returns it.
+  list<T> BacktracePath() const;
 
  private:
   T state;
   double cost;
-  State<T> previous_state;
+  State<T> *previous_state;
 };
 
 #endif //EX4__STATE_H_
